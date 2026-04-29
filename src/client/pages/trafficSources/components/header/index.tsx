7723261bd324a18db04c09e221d84352262d9024
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2026-04-29 01:38:48 -0300

    feat(header): enhance token copy functionality and update layout styles
    - Added a copy token feature that allows users to copy their token
      to the clipboard with visual feedback.
    - Introduced an IconButton for copying the token, replacing the
      previous Button.
    - Updated the layout styles to improve spacing and alignment
      within the header component.
    - Conditional rendering for the "Add traffic source" button based
      on user email.

--- src/client/pages/trafficSources/components/header/index.tsx
+++ src/client/pages/trafficSources/components/header/index.tsx
@@ -1,22 +1,55 @@
-import { Button, Tooltip, useModal } from "@arkyn/components";
-import { Plus } from "lucide-react";
+import { Button, IconButton, Tooltip, useModal } from "@arkyn/components";
+import { Check, Copy, Plus } from "lucide-react";
 import { Container } from "./styles";
+import { useLayout } from "~/client/hooks/useLayout";
+import { useState } from "react";
 
 function Header() {
+  const { user } = useLayout();
   const { openModal } = useModal("create-traffic-Source");
+
+  const [handleCopySuccess, setHandleCopySuccess] = useState(false);
+
+  function copyToken() {
+    navigator.clipboard.writeText(user?.token || "");
+    setHandleCopySuccess(true);
+
+    setTimeout(() => {
+      setHandleCopySuccess(false);
+    }, 2000);
+  }
 
   return (
     <Container>
       <h1>Traffic sources</h1>
 
-      <Tooltip
-        text="Your plan does not allow you to add traffic sources. Please upgrade to add traffic sources."
-        orientation="left"
-      >
-        <Button leftIcon={Plus} onClick={openModal} disabled>
+      <Tooltip text="Copy token to clipboard">
+        <IconButton
+          aria-label="Copy tokenButton"
+          variant="outline"
+          onClick={copyToken}
+          icon={handleCopySuccess ? Check : Copy}
+          scheme={handleCopySuccess ? "success" : "primary"}
+          disabled={handleCopySuccess || !user?.token}
+        />
+      </Tooltip>
+
+      {user.email === "lucasgoncalves@arkyn.dev" && (
+        <Button leftIcon={Plus} onClick={openModal}>
           Add traffic source
         </Button>
-      </Tooltip>
+      )}
+
+      {user.email !== "lucasgoncalves@arkyn.dev" && (
+        <Tooltip
+          text="Your plan does not allow you to add traffic sources. Please upgrade to add traffic sources."
+          orientation="left"
+        >
+          <Button leftIcon={Plus} onClick={openModal} disabled>
+            Add traffic source
+          </Button>
+        </Tooltip>
+      )}
     </Container>
   );
 }
--- src/client/pages/trafficSources/components/header/styles.ts
+++ src/client/pages/trafficSources/components/header/styles.ts
@@ -3,13 +3,14 @@ import { styled } from "@linaria/react";
 const Container = styled.header`
   display: flex;
   align-items: start;
-  justify-content: space-between;
+  gap: 16px;
 
   h1 {
     font-weight: 600;
     font-size: 24px;
     line-height: 24px;
     color: var(--text-heading);
+    margin-right: auto;
   }
 `;
 
